b21dcaab61245c373463cc9640a56dcc72fda458
yoongyo <jyg0172@naver.com>
2019-11-12 15:44:58 +0900

    AD

--- contact/schema.py
+++ contact/schema.py
@@ -1,6 +1,6 @@
 import graphene
 from graphene_django.types import DjangoObjectType
-from .models import Contact
+from .models import Contact, Item
 
 
 class ContactType(DjangoObjectType):
@@ -8,8 +8,18 @@ class ContactType(DjangoObjectType):
         model = Contact
 
 
+class ItemType(DjangoObjectType):
+    class Meta:
+        model = Item
+
+
 class Query(graphene.AbstractType):
     all_contact = graphene.List(ContactType)
 
+    all_item = graphene.List(ItemType)
+
     def resolve_all_contacts(self, context, **kwargs):
         return Contact.objects.all()
+
+    def resolve_all_item(self, context, **kwargs):
+        return Item.objects.all()
